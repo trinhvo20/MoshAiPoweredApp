ef9a1a4763b4c975d6642c231526a7faf0c14710
Trinh Vo <uhttv@sfbcic.com>
2025-12-18 11:04:28 -0600

    Build the Chat API (continue)

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
-import { resolve } from 'bun';
 
 // Store all variables in .env as Environment Variables
 dotenv.config();
@@ -28,16 +27,22 @@ app.get('/api/hello', (req: Request, res: Response) => {
 });
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-    const {prompt} = req.body; // grab the user's chat
-    // and send to OpenAI
-    const response = await client.responses.create({
-        model: 'gpt-40-mini',
-        input: prompt,
-        temperature: 0.2, // decide how logic/creative the answer is (0.2=logic, 1.0=creative)
-        max_output_tokens: 100 // tokens
-    })
-    // send the answer to user
-    res.json({message: response.output_text})
+    try {
+        const {prompt} = req.body; // grab the user's chat
+        // and send to OpenAI
+        const response = await client.responses.create({
+            model: 'gpt-4o-mini',
+            input: prompt,
+            temperature: 0.2, // decide how logic/creative the answer is (0.2=logic, 1.0=creative)
+            max_output_tokens: 100 // tokens
+        })
+        // send the answer to user
+        res.json({message: response.output_text})
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: "OpenAI request failed"})
+    }
+
 })
 
 app.listen(port, () => {
