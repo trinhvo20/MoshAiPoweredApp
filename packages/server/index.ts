3ab7d6c2f1f0c7920a2f59b7dca8938e5bc34f64
Trinh Vo <62305589+trinhvo20@users.noreply.github.com>
2025-12-29 14:54:32 -0600

    Input (chat) validation

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 // Store all variables in .env as Environment Variables
 dotenv.config();
@@ -30,9 +31,22 @@ app.get('/api/hello', (req: Request, res: Response) => {
 // converstionId -> lastResponseId in that conversation
 const conversations = new Map<string, string>();
 
+// input validation (chat)
+const chatSchema = z.object({
+    prompt: z.string().trim().min(1, 'Prompt is required').max(1000, 'Prompt is too long (max 1000 characters)'),
+    conversationId: z.string().uuid()
+})
+
 app.post('/api/chat', async (req: Request, res: Response) => {
     try {
-        const {prompt, conversationId} = req.body; // grab the user's chat
+        // validate the user's chat
+        const parsedResult = chatSchema.safeParse(req.body);
+        if (!parsedResult.success) {
+            return res.status(400).json(parsedResult.error.format());
+        }
+        // grab the user's chat
+        const {prompt, conversationId} = req.body; 
+        
         // and send to OpenAI
         const response = await client.responses.create({
             model: 'gpt-4o-mini',
@@ -41,8 +55,10 @@ app.post('/api/chat', async (req: Request, res: Response) => {
             max_output_tokens: 100, // tokens
             previous_response_id: conversations.get(conversationId)
         })
+        
         // update the chatbox's memory
         conversations.set(conversationId, response.id);
+        
         // send the answer to user
         res.json({message: response.output_text})
     } catch (err) {
